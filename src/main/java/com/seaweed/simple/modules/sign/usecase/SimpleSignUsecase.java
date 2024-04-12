418e56d2af4094ca5be3f6c42142c7c771e851cd
ihk <ihk@satreci.com>
2024-04-12 16:52:31 +0900

    1. usecase 어노테이션 추가

--- src/main/java/com/seaweed/simple/modules/sign/usecase/SimpleSignUsecase.java
+++ src/main/java/com/seaweed/simple/modules/sign/usecase/SimpleSignUsecase.java
@@ -6,7 +6,9 @@ import com.seaweed.simple.modules.sign.domain.SimpleSign;
 import com.seaweed.simple.modules.sign.dto.SimpleSignDTO;
 import com.seaweed.simple.modules.user.service.SimpleUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SimpleSignUsecase {
 
     @Autowired
